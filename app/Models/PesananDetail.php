a775d5aa54b6b5883220a7aaa35a225d0bbb15b0
hanashofiyah <hana.shofiyah23@gmail.com>
2022-05-15 18:49:42 +0700

    first commit

---
+++ app/Models/PesananDetail.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PesananDetail extends Model
+{
+    public function barang(){
+        return $this->belongsTo('App\Barang','barang_id','id');
+    }
+    public function pesanan(){
+        return $this->belongsTo('App\Pesanan','pesanan_id','id');
+    }
+}
