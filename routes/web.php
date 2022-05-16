935f9d2493181232c7e474a5a04e2a8db315894b
ilhamn65 <ilhamnuansa65@gmail.com>
2022-05-17 02:23:24 +0700

    add all controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('pesan/{id}', 'PesanController@index');
+Route::post('pesan/{id}', 'PesanController@pesan');
+Route::get('check-out', 'PesanController@check_out');
+Route::delete('check-out/{id}', 'PesanController@delete');
+
+Route::get('konfirmasi-check-out', 'PesanController@konfirmasi');
+
+Route::get('profile', 'ProfileController@index');
+Route::post('profile', 'ProfileController@update');
+
+Route::get('history', 'HistoryController@index');
+Route::get('history/{id}', 'HistoryController@detail');
